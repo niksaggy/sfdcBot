706224bb0d71a0daf9a5e0795ffa3a50d30f343e
Sagnik <60902566+niksaggy@users.noreply.github.com>
2020-02-11 16:19:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 const jsforce = require('jsforce');
 const { dialogflow } = require('actions-on-google');
 const {
