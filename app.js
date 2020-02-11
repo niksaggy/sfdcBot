1714758f35e9257f176a0ec6a4e9f17a018c1626
sagnikRahaNL <saraha@deloitte.nl>
2020-02-11 13:10:56 +0100

    another commit

--- app.js
+++ app.js
@@ -1,9 +1,46 @@
-var express = require(‘express’);
+const express = require('express');
+const jsforce = require('jsforce');
+const { dialogflow } = require('actions-on-google');
+const {
+  SimpleResponse,
+  BasicCard,
+  Image,
+  Suggestions,
+  Button
+} = require('actions-on-google');
+
 var port = process.env.PORT || 3000;
-var app = express();
-app.get(‘/’, function (req, res) {
- res.send(JSON.stringify({ Hello: ‘World’}));
+var conn = new jsforce.Connection({
+  loginUrl : 'https://login.salesforce.com'
+});
+
+conn.login(process.env.username, process.env.password, function(err, userInfo) {
+	if (err) { 
+		return console.error(err); 
+	}
+	console.log(conn.accessToken);
+	console.log(conn.instanceUrl);
+	// logged in user property
+	console.log("User ID: " + userInfo.id);
+	console.log("Org ID: " + userInfo.organizationId);
+});
+
+
+
+
+//app instance
+const app = dialogflow();
+app.intent('Default Welcome Intent', (conv) => {
+	
+	conv.ask(new SimpleResponse({
+		speech:'Hi, how is it going? You are now logged into your personal dev org',
+		text:'Hi, how is it going? You are now logged into your personal dev org',
+	}));
+});
+
+app.get('/', function (req, res) {
+ res.send('Hello World!');
 });
 app.listen(port, function () {
- console.log(`Example app listening on port !`);
+ console.log('Example app listening on port !');
 });
