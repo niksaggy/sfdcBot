9d471d85ad6bf95b3355c2d6a3c26235799b484a
Sagnik <60902566+niksaggy@users.noreply.github.com>
2020-03-06 21:58:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -30,34 +30,7 @@ var oauth2 = new jsforce.OAuth2({
   redirectUri : 'https://salesforcebot-qjksum.firebaseapp.com/__/auth/handler'
 });
 
-//
-// Get authorization url and redirect to it.
-//
 
-expApp.get('/oauth2/auth', function(req, res) {
-  res.redirect(oauth2.getAuthorizationUrl({ scope : 'api id web' }));
-});
-
-//
-// Pass received authorization code and get access token
-//
-expApp.get('/oauth2/callback', function(req, res) {
-  var conn = new jsforce.Connection({ oauth2 : oauth2 });
-  var code = req.param('code');
-  conn.authorize(code, function(err, userInfo) {
-    if (err) { return console.error(err); }
-    // Now you can get the access token, refresh token, and instance URL information.
-    // Save them to establish connection next time.
-    console.log(conn.accessToken);
-    console.log(conn.refreshToken);
-    console.log(conn.instanceUrl);
-    console.log("User ID: " + userInfo.id);
-    console.log("Org ID: " + userInfo.organizationId);
-    // ...
-    res.send('success'); // or your desired response
-	options = { Authorization: 'Bearer '+conn.accessToken};
-  });
-});
 
 
 /*conn.login(process.env.username, process.env.password, function(err, userInfo) {
@@ -154,6 +127,34 @@ app.intent('Default Welcome Intent', (conv) => {
 		speech:'Hi, how is it going? You are being guided to the login page',
 		text:'Hi, how is it going? You are being guided to the login page',
 	}));
+	//
+	// Get authorization url and redirect to it.
+	//
+
+	expApp.get('/oauth2/auth', function(req, res) {
+	  res.redirect(oauth2.getAuthorizationUrl({ scope : 'api id web' }));
+	});
+
+	//
+	// Pass received authorization code and get access token
+	//
+	expApp.get('/oauth2/callback', function(req, res) {
+	  var conn = new jsforce.Connection({ oauth2 : oauth2 });
+	  var code = req.param('code');
+	  conn.authorize(code, function(err, userInfo) {
+		if (err) { return console.error(err); }
+		// Now you can get the access token, refresh token, and instance URL information.
+		// Save them to establish connection next time.
+		console.log(conn.accessToken);
+		console.log(conn.refreshToken);
+		console.log(conn.instanceUrl);
+		console.log("User ID: " + userInfo.id);
+		console.log("Org ID: " + userInfo.organizationId);
+		// ...
+		res.send('success'); // or your desired response
+		options = { Authorization: 'Bearer '+conn.accessToken};
+	  });
+	});
 });
 
 app.intent('Get Opportunity Info', (conv, {oppName,fieldNames} ) => {
