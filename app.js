76a9050bfadaa46177d21181f6b665a3cfc9ef70
Sagnik <60902566+niksaggy@users.noreply.github.com>
2020-03-07 15:50:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ var updateOppty = function(fieldNames,fieldValues,oppName){
 
 app.intent('Default Welcome Intent', (conv) => {
 	
-	app.get('/oauth2/auth', function(req, res) {
+	expApp.get('/oauth2/auth', function(req, res) {
 	const oauth2 = new jsforce.OAuth2({
 		clientId: process.env.SALESFORCE_CONSUMER_KEY,
 		clientSecret: process.env.SALESFORCE_CONSUMER_SECRET,
@@ -137,7 +137,7 @@ app.intent('Default Welcome Intent', (conv) => {
 	//
 	// Pass received authorization code and get access token
 	//
-	app.get('/getAccessToken', function(req,res) {
+	expApp.get('/getAccessToken', function(req,res) {
 		const oauth2 = new jsforce.OAuth2({
 		clientId: process.env.SALESFORCE_CONSUMER_KEY,
 		clientSecret: process.env.SALESFORCE_CONSUMER_SECRET,
