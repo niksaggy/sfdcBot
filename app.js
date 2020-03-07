2cff17f42eb26706744cfff2de56adbcc46b5b19
Sagnik <60902566+niksaggy@users.noreply.github.com>
2020-03-07 15:04:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -32,43 +32,7 @@ const app = dialogflow({
 // Get authorization url and redirect to it.
 //
 
-expApp.get('/oauth2/auth', function(req, res) {
-	const oauth2 = new jsforce.OAuth2({
-		clientId: process.env.SALESFORCE_CONSUMER_KEY,
-		clientSecret: process.env.SALESFORCE_CONSUMER_SECRET,
-		redirectUri: process.env.REDIRECT_URI
-	});
-	res.redirect(oauth2.getAuthorizationUrl({}));
-});
 
-//
-// Pass received authorization code and get access token
-//
-expApp.get('/getAccessToken', function(req,res) {
-  const oauth2 = new jsforce.OAuth2({
-    clientId: process.env.SALESFORCE_CONSUMER_KEY,
-    clientSecret: process.env.SALESFORCE_CONSUMER_SECRET,
-    redirectUri: process.env.REDIRECT_URI
-  });
-  const conn = new jsforce.Connection({ oauth2 : oauth2 });
-  conn.authorize(req.query.code, function(err, userInfo) {
-    if (err) {
-      return console.error(err);
-    }
-    const conn2 = new jsforce.Connection({
-      instanceUrl : conn.instanceUrl,
-      accessToken : conn.accessToken
-    });
-    conn2.identity(function(err, res) {
-      if (err) { return console.error(err); }
-      console.log("user ID: " + res.user_id);
-      console.log("organization ID: " + res.organization_id);
-      console.log("username: " + res.username);
-      console.log("display name: " + res.display_name);
-	  options = { Authorization: 'Bearer '+conn.accessToken};
-    });
-  });
-});
 
 
 /*conn.login(process.env.username, process.env.password, function(err, userInfo) {
@@ -160,6 +124,45 @@ var updateOppty = function(fieldNames,fieldValues,oppName){
 
 
 app.intent('Default Welcome Intent', (conv) => {
+	
+	expApp.get('/oauth2/auth', function(req, res) {
+	const oauth2 = new jsforce.OAuth2({
+		clientId: process.env.SALESFORCE_CONSUMER_KEY,
+		clientSecret: process.env.SALESFORCE_CONSUMER_SECRET,
+		redirectUri: process.env.REDIRECT_URI
+	});
+	res.redirect(oauth2.getAuthorizationUrl({}));
+	});
+
+	//
+	// Pass received authorization code and get access token
+	//
+	expApp.get('/getAccessToken', function(req,res) {
+		const oauth2 = new jsforce.OAuth2({
+		clientId: process.env.SALESFORCE_CONSUMER_KEY,
+		clientSecret: process.env.SALESFORCE_CONSUMER_SECRET,
+		redirectUri: process.env.REDIRECT_URI
+		});
+		
+		const conn = new jsforce.Connection({ oauth2 : oauth2 });
+		conn.authorize(req.query.code, function(err, userInfo) {
+			if (err) {
+			  return console.error(err);
+			}
+			const conn2 = new jsforce.Connection({
+			  instanceUrl : conn.instanceUrl,
+			  accessToken : conn.accessToken
+			});
+			conn2.identity(function(err, res) {
+			  if (err) { return console.error(err); }
+			  console.log("user ID: " + res.user_id);
+			  console.log("organization ID: " + res.organization_id);
+			  console.log("username: " + res.username);
+			  console.log("display name: " + res.display_name);
+			  options = { Authorization: 'Bearer '+conn.accessToken};
+			});
+		});
+	});
 	
 	conv.ask(new SimpleResponse({
 		speech:'Hi, how is it going? You are being guided to the login page',
