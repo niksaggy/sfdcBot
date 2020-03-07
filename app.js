667d7037193a7e0fb98d82728e81317ce4ca0b38
Sagnik <60902566+niksaggy@users.noreply.github.com>
2020-03-07 11:39:12 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,9 +12,8 @@ const {
 
 var options;
 var port = process.env.PORT || 3000;
-var conn = new jsforce.Connection({
-  loginUrl : 'https://login.salesforce.com'
-});
+
+var conn = new jsforce.Connection({ oauth2 : oauth2 });
 
 const expApp = express().use(bodyParser.json());
 
@@ -30,7 +29,34 @@ var oauth2 = new jsforce.OAuth2({
   redirectUri : 'https://sfdcadminbot.herokuapp.com/token'
 });
 
+//
+// Get authorization url and redirect to it.
+//
 
+expApp.get('/oauth2/auth', function(req, res) {
+  res.redirect(oauth2.getAuthorizationUrl({ scope : 'api id web' }));
+});
+
+//
+// Pass received authorization code and get access token
+//
+expApp.get('/oauth2/callback', function(req, res) {
+  //var conn = new jsforce.Connection({ oauth2 : oauth2 });
+  var code = req.param('code');
+  conn.authorize(code, function(err, userInfo) {
+	if (err) { return console.error(err); }
+	// Now you can get the access token, refresh token, and instance URL information.
+	// Save them to establish connection next time.
+	console.log(conn.accessToken);
+	console.log(conn.refreshToken);
+	console.log(conn.instanceUrl);
+	console.log("User ID: " + userInfo.id);
+	console.log("Org ID: " + userInfo.organizationId);
+	// ...
+	res.send('success'); // or your desired response
+	options = { Authorization: 'Bearer '+conn.accessToken};
+  });
+});
 
 
 /*conn.login(process.env.username, process.env.password, function(err, userInfo) {
@@ -127,34 +153,6 @@ app.intent('Default Welcome Intent', (conv) => {
 		speech:'Hi, how is it going? You are being guided to the login page',
 		text:'Hi, how is it going? You are being guided to the login page',
 	}));
-	//
-	// Get authorization url and redirect to it.
-	//
-
-	expApp.get('/oauth2/auth', function(req, res) {
-	  res.redirect(oauth2.getAuthorizationUrl({ scope : 'api id web' }));
-	});
-
-	//
-	// Pass received authorization code and get access token
-	//
-	expApp.get('/oauth2/callback', function(req, res) {
-	  var conn = new jsforce.Connection({ oauth2 : oauth2 });
-	  var code = req.param('code');
-	  conn.authorize(code, function(err, userInfo) {
-		if (err) { return console.error(err); }
-		// Now you can get the access token, refresh token, and instance URL information.
-		// Save them to establish connection next time.
-		console.log(conn.accessToken);
-		console.log(conn.refreshToken);
-		console.log(conn.instanceUrl);
-		console.log("User ID: " + userInfo.id);
-		console.log("Org ID: " + userInfo.organizationId);
-		// ...
-		res.send('success'); // or your desired response
-		options = { Authorization: 'Bearer '+conn.accessToken};
-	  });
-	});
 });
 
 app.intent('Get Opportunity Info', (conv, {oppName,fieldNames} ) => {
